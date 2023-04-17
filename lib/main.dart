e419b93d847e3d8b32b8a91af70feed4cf34e402
Carlos Callejas <carlocallejas91@outlook.com>
2023-04-17 11:45:06 -0300

    main: added firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import "package:firebase_core/firebase_core.dart";
 import "package:flutter/material.dart";
-import 'package:get/get.dart';
-import 'package:page_penner/core/theme/app_theme.dart';
-import 'package:page_penner/core/values/strings.dart';
-import 'package:page_penner/routes/app_pages.dart';
-import 'package:page_penner/routes/app_routes.dart';
+import "package:get/get.dart";
+import "package:page_penner/core/theme/app_theme.dart";
+import "package:page_penner/core/values/strings.dart";
+import "package:page_penner/firebase_options.dart";
+import "package:page_penner/routes/app_pages.dart";
+import "package:page_penner/routes/app_routes.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
